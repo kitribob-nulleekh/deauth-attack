972d7d2b408516dd638df3aac2b64ec40f93ade6
nulLeeKH <i_am@nulleekh.com>
2021-02-03 22:22:39 +0900

    ftr: Add pcap handle

--- src/main.cpp
+++ src/main.cpp
@@ -14,4 +14,12 @@ int main(int argc, char** argv) {
 		Usage(argv[0]);
 		return -1;
 	}
+
+	char* dev = argv[1];
+	char errBuf[PCAP_ERRBUF_SIZE];
+	pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errBuf);
+	if (handle == nullptr) {
+		printf("FATAL: Couldn't open device %s(%s)\n", dev, errBuf);
+		return -1;
+	}
 }
