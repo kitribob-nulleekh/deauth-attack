227f2f5a77d51138aefc4601a0db930c902c2525
nulLeeKH <i_am@nulleekh.com>
2021-02-03 22:30:31 +0900

    ftr: Add functions which send packet

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,20 @@ void Usage(char* arg) {
 	printf("sample: %s mon0 00:11:22:33:44:55 66:77:88:99:AA:BB\n", arg);
 }
 
+void SendBroadcastDeauth(pcap_t* handle, Mac apMac) {
+	// make packet	
+	while (true) {
+		// send packet
+	}
+}
+
+void SendUnicastDeauth(pcap_t* handle, Mac apMac, Mac stationMac) {
+        // make packet
+        while (true) {
+                // send packet
+        }
+}
+
 int main(int argc, char** argv) {
 	if (3 != argc && 4 != argc) {
 		Usage(argv[0]);
@@ -22,4 +36,15 @@ int main(int argc, char** argv) {
 		printf("FATAL: Couldn't open device %s(%s)\n", dev, errBuf);
 		return -1;
 	}
+
+
+	if (3 == argc) {
+		Mac arg2(argv[2]);
+		SendBroadcastDeauth(handle, arg2);
+	} else {
+		Mac arg2(argv[2]), arg3(argv[3]);
+		SendUnicastDeauth(handle, arg2, arg3);
+	}
+
+	pcap_close(handle);
 }
