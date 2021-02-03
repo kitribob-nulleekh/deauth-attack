2327d5e1f472cb9ef48876a333d6e61960ce9e59
nulLeeKH <i_am@nulleekh.com>
2021-02-03 22:19:43 +0900

    ftr: Add Uage function

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 #include "packet.h"
 #include "mac.h"
 
+
+void Usage(char* arg) {
+	printf("syntax: %s <interface> <ap mac> [<station mac>]\n", arg);
+	printf("sample: %s mon0 00:11:22:33:44:55 66:77:88:99:AA:BB\n", arg);
+}
+
 int main(int argc, char** argv) {
-	printf("Hello, World!");
+	if (3 != argc && 4 != argc) {
+		Usage(argv[0]);
+		return -1;
+	}
 }
